6c55aa7f4771be6b0506e9ab82640f8b38295731
Colby Bates <batesbobby.1@netzero.net>
2016-11-17 15:29:35 -0800

    Initial commit

---
+++ node.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: Colby Bates
+ * File: node.cpp
+ */
+
+#include "node.h"
+//Constructor
+Node::Node(int value) : id(value), cur_color("Empty")
+{
+}
+//Destructor
+Node::~Node()
+{
+
+}
+
+void Node::set_color(string value)
+{
+  cur_color = value;
+}
+
+string Node::get_cur_color()
+{
+  return cur_color;
+}
+
+vector<string> Node::get_crossed_out()
+{
+  return crossed_out;
+}
---
+++ node.h
@@ -0,0 +1,25 @@
+/*
+ * Author: Colby Bates
+ * File: node.h
+ */
+
+#ifndef NODE_H
+#define NODE_H
+
+#include<vector>
+#include<string>
+
+class Node
+{
+  public:
+    Node(int num);
+    ~Node();
+    void set_color(string value);
+    string get_cur_color();
+    vector<string> get_crossed_out();
+  
+  private:
+    int id;
+    string cur_color;
+    vector<string> crossed_out;
+}
