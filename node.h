fbb12ac669e49b7e55f252e8b17863836bacbdca
Colby Bates <batesbobby.1@netzero.net>
2016-11-17 17:33:07 -0800

    Changed node class to camel casing

--- node.h
+++ node.h
@@ -17,16 +17,16 @@ class Node
   public:
     Node(int num);
     ~Node();
-    void set_color(string value);
-    string get_cur_color();
-    vector<string> get_crossed_out();
-    void cross_out_neighbors(Node* neighbor);
+    void setColor(string value);
+    string getCurColor();
+    vector<string> getCrossedOut();
+    void crossOutNeighbors(Node* neighbor);
   
   private:
-    int id;
-    string cur_color;
-    vector<string> crossed_out;
-    vector<Node*> crossed_neighbors;
+    int m_id;
+    string m_curColor;
+    vector<string> m_crossedOut;
+    vector<Node*> m_crossedNeighbors;
 };
 
 #endif
