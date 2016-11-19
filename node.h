f32a8f99444677defcc2c0acdbd13cab0924bcbd
Colby Bates <batesbobby.1@netzero.net>
2016-11-18 16:34:47 -0800

    added header for ifCrossedOut

--- node.h
+++ node.h
@@ -26,6 +26,7 @@ class Node
     void undoCrossedOut();
     void freeColor(string color);
     void addCrossedOut(string color);
+    bool ifCrossedOut(string color); 
   
   private:
     int m_id;
