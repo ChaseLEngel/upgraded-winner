29df18c2e4a7442190f32fa420683d53cf61da1c
ChaseLEngel <chaselengel@gmail.com>
2016-11-25 11:16:31 -0800

    Working on updating node color.

--- gui/gui.rb
+++ gui/gui.rb
@@ -2,20 +2,44 @@
 require 'gtk3'
 
 class Node
+  attr_accessor :x, :y
   def initialize(cairo, id, x, y)
-    @cairo = cairo
-    drawNode(x, y)
-    #@cairo.move_to x, y
-    #@cairo.show_text id
+    @x = x
+    @y = y
+    @id = id
+    drawNode(cairo)
+    setId(cairo)
+  end
+
+  def link(cairo, node)
+    cairo.move_to @x, @y
+    cairo.line_to node.x, node.y
+    cairo.stroke
+  end
+
+  def setColor(cairo, color)
+    cairo.move_to @x, @y
+    cairo.set_source_rgb(0.3, 0.2, 0.6)
+    cairo.fill
   end
 
   private
-  def drawNode(x, y)
+
+  def drawNode(cairo)
+    radius = 20
+    cairo.set_source_rgb(0.1, 0.1, 0.2)
     # Draw a circle
-    arc = @cairo.arc x, y, 20, 0, 2*Math::PI
+    cairo.arc @x, @y, radius, 0, 2*Math::PI
     # Draw outline of circle
-    arc.stroke
+    cairo.stroke
   end
+
+  def setId(cairo)
+    cairo.move_to @x, @y
+    cairo.show_text @id
+    cairo.stroke
+  end
+
 end
 
 # Create new Gtk window instance
@@ -26,13 +50,22 @@ window.set_title 'Graph Coloring'
 # When window close button is clicked, exit program.
 window.signal_connect('destroy') { Gtk.main_quit }
 
-WIN_X = 200
-WIN_Y = 200
+WIN_X = 500
+WIN_Y = 800
 window.set_default_size WIN_X, WIN_Y
 window.set_window_position :center
 
 # Create new drawing space.
 draw_window = Gtk::DrawingArea.new
+
+draw_window.queue_draw_area(0, 0, WIN_X, WIN_Y)
+positions = [
+  {x: 40, y: 40},
+  {x: 80, y: 80},
+  {x: 120, y: 80},
+]
+
+nodes = []
 
 # Listen for Gtk to call draw event.
 draw_window.signal_connect('draw') do 
@@ -41,15 +74,21 @@ draw_window.signal_connect('draw') do
   @cr.set_font_size 13 
 
   # Draw nodes and edges
-end
-
-draw_window.add_tick_callback do |_, frame_clock|
-  # Update node colors
-  Node.new(@cr, '0', rand(20..WIN_X-20), rand(20..WIN_Y-20))
-  draw_window.queue_draw
+  nodes << Node.new(@cr, '0', positions[0][:x], positions[0][:y])
+  nodes << Node.new(@cr, '1', positions[1][:x], positions[1][:y])
+  true
 end
 
 window.add draw_window
+
+draw_window.add_tick_callback do |_, frame_clock|
+  # Update node colors
+  n = Node.new(@cr, '2', rand(WIN_X), rand(WIN_Y) )
+  n.setColor(@cr, :red)
+end
+
 window.show_all
 
+
 Gtk.main
+
