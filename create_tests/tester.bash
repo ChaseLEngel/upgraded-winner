5cb47ff03f6b20fd0d4ecd6f3785a10f5eaf104f
Thomas Greenman <tgreenman@mail.csuchico.edu>
2016-11-30 10:59:06 -0800

    Minor testing tweaks

--- create_tests/tester.bash
+++ create_tests/tester.bash
@@ -1,18 +1,19 @@
-#/usr/bin/bash
-
 echo "Running tester"
 accept_count=0
 reject_count=0
-for i in {0..1000}
+for j in {0..5}
 do
-  ./script.rb 10 3 > out
-  ./wumpus out > /dev/null
-  if [ $? -eq 0 ]
-  then
-    accept_count=$((accept_count+1))
-  else
-    reject_count=$((reject_count+1))
-  fi
+  for i in {0..1000}
+  do
+    ./script.rb 5 3 > out
+    ./wumpus out > /dev/null
+    if [ $? -eq 0 ]
+    then
+      accept_count=$((accept_count+1))
+    else
+      reject_count=$((reject_count+1))
+    fi
+  done
 done
 echo "Accepted: " $accept_count
 echo "Rejected: " $reject_count
